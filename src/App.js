79940a8c4f185fb2a4573ef666aad7cfdb27d402
RShtiltshen <f15her.ny@gmail.com>
2020-07-05 11:11:22 +0300

    Add scramble field to desktop (without generation)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Timer} from './components/Timer';
+import {Scramble} from './components/Scramble';
 
 function App() {
   return (
     <div className="App">
+      <Scramble/>
       <Timer/>
     </div>
   );
---
+++ src/components/Scramble/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './scramble.module.scss';
+
+export const Scramble = () => {
+  return (
+    <div id={s.scramble}>
+       R R' R llLL l l sidf 
+    </div>
+  )
+}
